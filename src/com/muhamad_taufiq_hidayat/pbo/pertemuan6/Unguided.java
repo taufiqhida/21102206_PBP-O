59308a2c6ae04de4ce5ee1ea1bd80761979d19cd
taufiqhida <56679957+taufiqhida@users.noreply.github.com>
2023-05-30 19:57:58 +0700

    unguided

---
+++ src/com/muhamad_taufiq_hidayat/pbo/pertemuan6/Unguided.java
@@ -0,0 +1,32 @@
+package com.muhamad_taufiq_hidayat.pbo.pertemuan6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Unguided {
+    public static int jumlahBilangan(int angka1, int angka2) {
+
+        return angka1 / angka2;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int hasil;
+        try {
+            System.out.print("Masukkan Bilangan Pertama ");
+            int angka1 = input.nextInt();
+            System.out.print("Masukkan Bilangan K5edua ");
+            int angka2 = input.nextInt();
+            hasil = jumlahBilangan(angka1, angka2);
+
+            System.out.println("Hasil" + hasil);
+
+        }catch (ArithmeticException e){
+            System.out.println("Error : "+e);
+            System.out.println("Terjadi keselahan: Input tidak valid. Harap masukkan bilangan bulat");
+        }catch (InputMismatchException e){
+            System.out.println("Error : "+e);
+            System.out.println("Terjadi kesalahan: Pembagian dengan 0");
+        }
+    }
+}
